0facd4b6ba168e9536246f8040b0c7a1c6718c78
abinonayq <abinonayq@elm.sa>
2019-03-05 18:33:22 +0300

    fierst PUSH !

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@
 /* template */
 
 
+
 var temp = '  <article class="article">'+
 '<section class="featuredImage">'+
   `<img src="#imgurl#" alt="" />`+
@@ -23,15 +24,29 @@ var temp = '  <article class="article">'+
 '<div class="clearfix"></div>'+
 '</article>';
 
-var contentDiv = "" ;
+
+var pupup= '<div class="container">'+
+  '<h1 id="title">#title#</h1>'+
+  '<h5 id="aouther">#aouther#</h5>'+
+  '<img style="width:100%" id="imgurl" src="#imgurl#" alt="" />'+
+  '<p id="cont">#cont#</p>'+
+  '<a href="#" class="popUpAction" target="_blank">Read more from source</a>'+
+'</div>';
+
 
 /* APIs */
+
+
+
+//---------- start API 1 -----------//
+$("#newsapi").on('click', function(){
 
 var url1 = 'https://newsapi.org/v2/top-headlines?' +
           'country=us&' +
           'apiKey=baddbb89804b4323b1bbb96a1c3f8b4e';
 var req = new Request(url1);
 var art;
+
 
 // debugger;
 $.ajax({
@@ -40,41 +55,50 @@ $.ajax({
     type:"GET",
    // data: {"country":"us" ,"apiKey":"baddbb89804b4323b1bbb96a1c3f8b4e" },
     success:function(data){
-      console.log(data.articles);
+      $(".article").remove();
+      // console.log(data.articles);
       art = data.articles;
-
+      var imgurl = '';
+      var auth = '';
+      var title = '';
+      var content = '';
+      var tempM ='';
       art.forEach(t => {
 // debugger;
-        var imgurl = t.urlToImage;
-        var auth = t.author;
-        var title = t.title;
-        var content = t.content;
-        var tempM = temp.replace("#title#",title).replace("#imgurl",imgurl).replace("#aouther#",auth).replace("#cont#",content) ;
-
-      $("#title").text(title)
-      $("#aouther").text(auth)      
-      $("#cont").text(content)
-      $("#imgurl").attr("src",imgurl);
+         imgurl = t.urlToImage;
+         auth = t.author;
+         title = t.title;
+         content = t.content;
+         tempM = temp.replace("#title#",title).replace("#imgurl#",imgurl).replace("#aouther#",auth).replace("#cont#",content) ;
+         pupup1 = pupup.replace("#title#",title).replace("#imgurl#",imgurl).replace("#aouther#",auth).replace("#cont#",content) 
+      // $("#title").text(title)
+      // $("#aouther").text(auth)      
+      // $("#cont").text(content)
+      // $("#imgurl").attr("src",imgurl);
 
       
-      
+$('#title2').on('click',function(){
+  $("#popUp").removeClass("hidden").removeClass("loader")
+  $("article").hide();
+    });
+
+$('.closePopUp').on('click',function(){
+  $('#popUp').addClass('hidden')       
+  $("article").show();
+});
+
         $(".articles").append(tempM)
+        $("#popUp").append(pupup1)
         // $("#popUp")
-        
-        $('#title2').on('click',function(){
-          $("#popUp").removeClass("hidden").removeClass("loader")
-          $("article").hide();
-            
-        });
-
-        $('.closePopUp').on('click',function(){
-          $('#popUp').addClass('hidden')       
-          $("article").show();
-        });
-        
-
-        
       });
+
+
+        
+
+        
+      
+
+
   
 
     },error:function(a,b,c){
@@ -83,12 +107,86 @@ $.ajax({
 
     
 })
-    
+});
+//-------- End API1 ---------- //
 
+//---------- Start API 2 -----------//
+
+$("#NYnews").on('click', function(){
+  $("article").hide();
+  var url2 = 'https://api.nytimes.com/svc/news/v3/content/all/all.json?api-key=GJlAIjuyZ00wUOIGmCZeWDbuEtuCnUuK' ;
+             
+ 
+  var art2;
+  $.ajax({
+  
+      url:url2,
+      type:"get",
+      data: { api_key : "GJlAIjuyZ00wUOIGmCZeWDbuEtuCnUuK"},
+      success:function(data2){
+
+         
+        console.log(data2.results);
+        art2 = data2.results;
+        var imgurl = '';
+        var auth = '';
+        var title = '';
+        var content = '';
+        var tempM ='';
+        art2.forEach(t => {
+  // // debugger;
+           imgurl = t.thumbnail_standard;
+           auth = t.byline;
+           title = t.title;
+           content = t.abstract;
+           tempM = temp.replace("#title#",title).replace("#imgurl#",imgurl).replace("#aouther#",auth).replace("#cont#",content) ;
+           pupup1 = pupup.replace("#title#",title).replace("#imgurl#",imgurl).replace("#aouther#",auth).replace("#cont#",content) 
+        $("#title").text(title)
+        $("#aouther").text(auth)      
+        $("#cont").text(content)
+        $("#imgurl").attr("src",imgurl);
+  
+        
+  $('#title2').on('click',function(){
+    $("#popUp").removeClass("hidden").removeClass("loader")
+    $("article").hide();
+      });
+  
+  $('.closePopUp').on('click',function(){
+    $('#popUp').addClass('hidden')       
+    $("article").show();
+  });
+  
+          $(".articles").append(tempM)
+          $("#popUp").append(pupup1)
+          // $("#popUp")
+        });
+  
+  
+          
+  
+          
+        
+  
+  
+    
+  
+      },error:function(a,b,c){
+        console.error(a,b,c);
+      }
+  
+      
+  })
+});
+
+  //-------- End API2 ---------- //
+
+
+    
   $('#searchIcon').on('click',function(){
     $('#searchInput').toggleClass('show-items');
     $('#source').toggleClass('hide-items');
   });
   
 
-console.log();
+

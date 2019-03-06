0ba333b94a48ca2434e81d9fbf564bfc9028b04c
abinonayq <abinonayq@elm.sa>
2019-03-06 17:06:22 +0300

    i

--- js/app.js
+++ js/app.js
@@ -1,9 +1,19 @@
 /*
   Please add all Javascript code to this file.
 */
+$(document).ready(function () {
+  getNY();
 
+$("#NYnews").on('click', function(){
+   getNY();
+});
 
+$("#newsapi").on('click', function(){
+  $("article").hide();
+  getNEWSAPI();
+});
 
+});
 
 /* template */
 
@@ -39,80 +49,121 @@ var pupup= '<div class="container">'+
 
 
 //---------- start API 1 -----------//
-$("#newsapi").on('click', function(){
+function getNEWSAPI() {
 
 var url1 = 'https://newsapi.org/v2/top-headlines?' +
           'country=us&' +
           'apiKey=baddbb89804b4323b1bbb96a1c3f8b4e';
-var req = new Request(url1);
+
 var art;
 
-
-// debugger;
 $.ajax({
+  
+  url:url1,
+  type:"get",
 
-    url:url1,
-    type:"GET",
-   // data: {"country":"us" ,"apiKey":"baddbb89804b4323b1bbb96a1c3f8b4e" },
-    success:function(data){
-      $(".article").remove();
-      // console.log(data.articles);
-      art = data.articles;
-      var imgurl = '';
-      var auth = '';
-      var title = '';
-      var content = '';
-      var tempM ='';
-      art.forEach(t => {
-// debugger;
-         imgurl = t.urlToImage;
+  success:function(data){
+
+     
+    console.log(data.articles);
+    art = data.articles;
+    var imgurl = '';
+    var auth = '';
+    var title = '';
+    var content = '';
+    var tempM ='';
+    let num = 0 ;
+    art.forEach(t => {
+      
+// // debugger;
+imgurl = t.urlToImage;
          auth = t.author;
          title = t.title;
          content = t.content;
          tempM = temp.replace("#title#",title).replace("#imgurl#",imgurl).replace("#aouther#",auth).replace("#cont#",content) ;
-         pupup1 = pupup.replace("#title#",title).replace("#imgurl#",imgurl).replace("#aouther#",auth).replace("#cont#",content) 
-      // $("#title").text(title)
-      // $("#aouther").text(auth)      
-      // $("#cont").text(content)
-      // $("#imgurl").attr("src",imgurl);
+    
 
-      
-$('#title2').on('click',function(){
-  $("#popUp").removeClass("hidden").removeClass("loader")
-  $("article").hide();
-    });
-
+  $("article").on("click", function () {
+    $("#popUp").removeClass("hidden").removeClass("loader")
+    $("article").hide();
+       
+    $("#title").text(`${t.title}`)
+    $("#aouther").text(`${t.author}`)
+    $("#cont").text(`${t.content}`)
+    // $(".popUpAction").attr('href', `${t.url}`)
+    $("#imgurl").attr(src,`${t.urlToImage}`)
+        
+  });      
 $('.closePopUp').on('click',function(){
-  $('#popUp').addClass('hidden')       
-  $("article").show();
-});
+$('#popUp').addClass('hidden')       
+$("article").show();
+});  
+      $(".articles").append(tempM)
+      // $("#popUp").append(pupup1)
+      // $("#popUp")
+    });
+  },error:function(a,b,c){
+    console.error(a,b,c);
+  }      
+})
+};
 
-        $(".articles").append(tempM)
-        $("#popUp").append(pupup1)
-        // $("#popUp")
-      });
+// debugger;
+// $.ajax({
 
+//     url:url1,
+//     type:"GET",
+//    // data: {"country":"us" ,"apiKey":"baddbb89804b4323b1bbb96a1c3f8b4e" },
+//     success:function(data){
+//       $(".article").remove();
+//       // console.log(data.articles);
+//       art = data.articles;
+//       var imgurl = '';
+//       var auth = '';
+//       var title = '';
+//       var content = '';
+//       var tempM ='';
+//       art.forEach(t => {
+// // debugger;
+//          imgurl = t.urlToImage;
+//          auth = t.author;
+//          title = t.title;
+//          content = t.content;
+//          tempM = temp.replace("#title#",title).replace("#imgurl#",imgurl).replace("#aouther#",auth).replace("#cont#",content) ;
+//          pupup1 = pupup.replace("#title#",title).replace("#imgurl#",imgurl).replace("#aouther#",auth).replace("#cont#",content) 
+//       // $("#title").text(title)
+//       // $("#aouther").text(auth)      
+//       // $("#cont").text(content)
+//       // $("#imgurl").attr("src",imgurl);
 
-        
-
-        
       
-
-
+// $('#title2').on('click',function(){
   
+//   $("#popUp").removeClass("hidden").removeClass("loader")
+//   $("article").hide();
+//     });
 
-    },error:function(a,b,c){
-      console.error(a,b,c);
-    }
+// $('.closePopUp').on('click',function(){
+//   $('#popUp').addClass('hidden')       
+//   $("article").show();
+// });
+
+//         $(".articles").append(tempM)
+//         $("#popUp").append(pupup1)
+//         // $("#popUp")
+//       });
+//     },error:function(a,b,c){
+//       console.error(a,b,c);
+//     }
 
     
-})
-});
+// });
+// }
 //-------- End API1 ---------- //
 
 //---------- Start API 2 -----------//
 
-$("#NYnews").on('click', function(){
+function getNY() {
   $("article").hide();
   var url2 = 'https://api.nytimes.com/svc/news/v3/content/all/all.json?api-key=GJlAIjuyZ00wUOIGmCZeWDbuEtuCnUuK' ;
              
@@ -133,51 +184,51 @@ $("#NYnews").on('click', function(){
         var title = '';
         var content = '';
         var tempM ='';
+        let num = 0 ;
         art2.forEach(t => {
+          
   // // debugger;
            imgurl = t.thumbnail_standard;
            auth = t.byline;
            title = t.title;
            content = t.abstract;
            tempM = temp.replace("#title#",title).replace("#imgurl#",imgurl).replace("#aouther#",auth).replace("#cont#",content) ;
-           pupup1 = pupup.replace("#title#",title).replace("#imgurl#",imgurl).replace("#aouther#",auth).replace("#cont#",content) 
-        $("#title").text(title)
-        $("#aouther").text(auth)      
-        $("#cont").text(content)
-        $("#imgurl").attr("src",imgurl);
+          //  pupup1 = pupup.replace("#title#",title).replace("#imgurl#",imgurl).replace("#aouther#",auth).replace("#cont#",content) ;
+        // $("#title").text(title)
+        // $("#aouther").text(auth)      
+        // $("#cont").text(content)
+        // $("#imgurl").attr("src",imgurl);
   
         
-  $('#title2').on('click',function(){
-    $("#popUp").removeClass("hidden").removeClass("loader")
-    $("article").hide();
-      });
+  // $('#title2').on('click',function(){
+
+  //     });
   
+      $("article").on("click", function () {
+        $("#popUp").removeClass("hidden").removeClass("loader")
+        $("article").hide();
+        console.log($(this).attr('id') - 1);
+        let index = $(this).attr('id') - 1
+        $("#title").text(`${t.title}`)
+        $("#aouther").text(`${t.byline}`)
+        $("#cont").text(`${t.abstract}`)
+        $(".popUpAction").attr('href', `${t.url}`)
+        $("#imgurl").attr(src,`${t.imgurl}`)
+            
+      });      
   $('.closePopUp').on('click',function(){
     $('#popUp').addClass('hidden')       
     $("article").show();
-  });
-  
+  });  
           $(".articles").append(tempM)
-          $("#popUp").append(pupup1)
+          // $("#popUp").append(pupup1)
           // $("#popUp")
         });
-  
-  
-          
-  
-          
-        
-  
-  
-    
-  
       },error:function(a,b,c){
         console.error(a,b,c);
-      }
-  
-      
+      }      
   })
-});
+};
 
   //-------- End API2 ---------- //
 
